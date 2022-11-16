1e94b4c492a00a81531c7fe4ee318ceeb2cb6a71
Nerada <5610242+Nerada@users.noreply.github.com>
2022-11-16 16:21:45 +0100

    Added SourceLink and more specific product versioning

---
+++ Dijkstra/buildMetaData.ps1
@@ -0,0 +1,9 @@
+param($officialBuild)
+
+$branch = git symbolic-ref --short -q HEAD
+$gitRef = git describe --always --abbrev=6 --dirty --exclude '*'
+$buildTime = [System.DateTimeOffset]::Now.ToString("o")
+
+if($officialBuild -ne "official-build"){$user = $env:UserName + "|"}
+
+$user + $branch + "|" + $gitRef + "|build-time_" + $buildTime + "_" + $test
