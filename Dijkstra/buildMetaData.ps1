fdc61b183a2499afd7e46dfb6ef8b86a916e08bd
Nerada <5610242+Nerada@users.noreply.github.com>
2022-11-17 10:18:59 +0100

    Add build script to project

--- Dijkstra/buildMetaData.ps1
+++
@@ -1,9 +0,0 @@
-param($officialBuild)
-
-$branch = git symbolic-ref --short -q HEAD
-$gitRef = git describe --always --abbrev=6 --dirty --exclude '*'
-$buildTime = [System.DateTimeOffset]::Now.ToString("o")
-
-if($officialBuild -ne "official-build"){$user = $env:UserName + "|"}
-
-$user + $branch + "|" + $gitRef + "|" + $buildTime
